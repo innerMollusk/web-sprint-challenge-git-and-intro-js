74715031be1de6d7995a5299db671e5b6c7493b6
Ben Johnson <monk.sound.design@gmail.com>
2023-04-05 21:15:19 -0400

    finished challenge, saved

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ const artists = [
     "name": "El Greco",
     "years": "1541 - 1614",
     "genre": "Mannerism",
-    "nationality": "Spanish,Greek",
+    "nationality": "Spanish, Greek",
     "bio": "Doménikos Theotokópoulos (Greek: Δομήνικος Θεοτοκόπουλος [ðoˈminikos θeotoˈkopulos]; October 1541 –  7 April 1614), most widely known as El Greco (\"The Greek\"), was a painter, sculptor and architect of the Spanish Renaissance. \"El Greco\" was a nickname, a reference to his Greek origin, and the artist normally signed his paintings with his full birth name in Greek letters, Δομήνικος Θεοτοκόπουλος, Doménikos Theotokópoulos, often adding the word Κρής Krēs, Cretan.",
     "wikipedia": "http://en.wikipedia.org/wiki/El_Greco",
     "paintings": 87
@@ -360,7 +360,7 @@ function artistByCountry(array, nationality){
   }
   return artistCountry;
 }
-artistByCountry(artists, "Russian");
+artistByCountry(artists, "Spanish");
 
 
 /* ***** END OF TASKS ***** */
